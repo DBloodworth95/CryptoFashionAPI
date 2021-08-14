8eaac26204dcbf2581fe93d76424226d2e6591db
dan <dan.bloodworth95@gmail.com>
2021-08-14 21:52:13 +0100

    Add domain & query pagination representation

---
+++ CryptoFashionAPI/Domain/PaginationFilter.cs
@@ -0,0 +1,9 @@
+namespace CryptoFashionAPI.Domain
+{
+    public class PaginationFilter
+    {
+        public int PageNumber { get; set; }
+        
+        public int PageSize { get; set; }
+    }
+}
